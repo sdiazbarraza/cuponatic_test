ed61dc8d473c591623bca413ca634762fa6db573
simp <simpdev7@gmail.com>
2017-11-02 16:09:41 -0300

    Fix migrations

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('search/{keyword}', 'WebService\ProductoController@search');
+Route::post('search', 'WebService\ProductoController@search');
