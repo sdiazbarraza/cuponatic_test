0829d6d41bd70719fbfc08ed3426c6d9ae944a55
simp <simpdev7@gmail.com>
2017-11-05 02:50:02 -0300

    Config set Enviroment3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') === 'production') {     URL::forceScheme('https'); }
+        if (env('APP_ENV') === 'production') {      $this->app['request']->server->set('HTTPS', true); }
     }
 
     /**
