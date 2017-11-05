ba47577efec5bce6dd5e4f9f140e674efba4edc3
simp <simpdev7@gmail.com>
2017-11-05 04:29:38 -0300

    Add cfg on production enviroment

--- config/database.php
+++ config/database.php
@@ -1,20 +1,7 @@
 <?php
 
 return [
-            $host = null;
-        $username = null;
-        $password = null;
-        $database = null;
 
-        if(!is_null(env('DATABASE_URL')))
-        {
-            $url = parse_url(env("DATABASE_URL"));
-
-            $host = $url["host"];
-            $username = $url["user"];
-            $password = $url["pass"];
-            $database = substr($url["path"], 1);
-        }
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
